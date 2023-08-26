3e21aab3d92e9e1a4944d8c387c5112f337db26e
xiehuan <1208044257@qq.com>
2023-08-26 19:16:39 +0800

    删除一部分注释

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import App from './App.vue'
 import '@/assets/iconfont.js';
 import loading from "./components/common/Loading"
 import message from "./components/common/Message"
-// import openSocket from './utils/Socket';
 import { createPinia } from 'pinia';
 const app=createApp(App)
 app.config.globalProperties.$loading = loading
 app.config.globalProperties.$message = message
-// app.config.globalProperties.$openSocket = openSocket
 app.use(router).use(createPinia())  .mount('#app')
