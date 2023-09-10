5518be00e8f0eaeb664af1ebf8303ef96e22e2e1
BINGWU <HuJiaCheng2003@163.com>
2023-09-10 23:30:55 +0800

    提交

---
+++ src/components/userLayout/Audio.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="audio"  v-show="auidoShow">
+  <div :class="['messg',audioBg]">
+    <soundWave :audioBg="audioBg"></soundWave>
+  </div>
+  <div class="footer"  >
+    <div 
+    :class="['esc',isActive==1?'avtive':'']"  ref="escRef" ></div>
+    <div  :class="['transformText',isActive==2?'avtive':'']"   ref="transformTextRef"></div>
+  </div>
+  </div>
+</template>
+
+<script setup>
+
+import { ref, onMounted,defineProps } from "vue"
+import Hammer from "hammerjs"
+import emitter from "@/utils/Bus"
+
+import soundWave from "../common/soundWave.vue"
+// 录音大组件控制显示
+const auidoShow =ref(false)
+// xdom
+const escRef=ref(null)
+// 文dom
+const transformTextRef=ref(null)
+// 控制是否选中
+const isActive=ref(0)
+
+defineProps({
+  audioBg:{
+    type:String,
+    default:"",
+  }
+
+})
+onMounted(()=>{
+  emitter.on("audioShow",(data)=>{
+    // 初始化
+    isActive.value=0
+    console.log("接收数据")
+    auidoShow.value=data
+
+  })
+  emitter.on("xWen",(data)=>{
+    console.log(data)
+    isActive.value=data
+
+  })
+  const hammer = new Hammer(document.querySelector(".audio"))
+ 
+  hammer.on("tap", (event) => {
+    console.log(event)
+    const rect = event.target.getBoundingClientRect()
+    console.log(rect)
+    console.log("触摸到了")
+  })
+
+})
+</script>
+
+<style scoped lang='scss'>
+.audio{
+  
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: calc(100% - 50px);
+  background: $mask-bg;
+  z-index: 999;
+  .messg{
+    display: flex;
+    padding: 5px 10px;
+    justify-content: center;
+    margin-bottom: 50px;
+  }
+  .left{
+    justify-content: flex-start!important;
+
+  }
+  .right{
+    justify-content: flex-end!important;
+  }
+  .footer{
+    height: 40%;
+    width: 100%;
+    >div{
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      background: $mask-yuan;
+      text-align: center;
+      line-height: 50px;
+      z-index: 555;
+    }
+    .esc{
+      left: 10%;
+      transform: rotateZ(-15deg);
+      &::before{
+        content: "X";
+      }
+    }
+  
+    .transformText{
+      transform: rotateZ(15deg);
+      right: 10%;
+      &:before{
+        content: "文";
+      }
+    }
+
+  }
+  .avtive{
+    background: #fff !important; ;
+  }
+}
+</style>
---
+++ src/components/userLayout/msgAudio.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="msgaudio" v-bind="$attrs">
+    <audio :src="wav" ref="audioMedia"  controls style="display: none;"></audio>
+
+            
+{{duration }}"
+    <!-- <div class="box">
+    <div class="wifi-symbol">
+      <div class="wifi-circle first"></div>
+      <div class="wifi-circle second"></div>
+      <div class="wifi-circle third"></div>
+      <div class="wifi-circle fourth"></div>
+    </div>
+  </div> -->
+  </div>
+</template>
+
+<script setup>
+import { ref,defineProps,watch ,useAttrs} from "vue"
+const attrs=useAttrs()
+console.log(attrs)
+const props=defineProps({
+  wav:{
+    type:String,
+    default:"",
+
+  },
+  play:{
+    type:Boolean,
+    default:false,
+  },
+  duration:{
+    type:Number,
+    default:0,
+  }
+})
+const audioMedia=ref(null)
+watch(()=>props.play,(newVal)=>{
+  if(newVal){
+    audioMedia.value.play()
+  }else{
+    audioMedia.value.pause()
+  }
+})
+</script>
+
+<style scoped lang='scss'>
+.msgaudio{
+  width: 50px;
+  height: 50px;
+  .box {
+  position: relative;
+  width: 240px;
+  height: 240px;
+  transform: rotate(-90deg) scale(.2);
+  
+.wifi-symbol {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+  transform: rotate(45deg);
+  position: absolute;
+  top: -30%;
+  .wifi-circle {
+  border: 20px solid #ccc;
+  border-radius: 50%;
+  position: absolute;
+ 
+}
+
+.first {
+  width: 140%;
+  height: 140%;
+  top: 30%;
+  left: 30%;
+  animation: identifier 1s 1s  forwards  ;
+}
+.second {
+  width: 100%;
+  height: 100%;
+  top: 50%;
+  left: 50%;
+  animation: identifier 1s 2s  forwards ;
+}
+.third {
+  width: 60%;
+  height: 60%;
+  top: 70%;
+  left: 70%;
+  animation: identifier 1s 3s   forwards;
+}
+.fourth {
+  width: 15%;
+  height: 15%;
+  top: 90%;
+  left: 90%;
+  animation: identifier 1s 4s   forwards;
+}
+}
+}
+}
+
+
+@keyframes identifier {
+  100%{
+    opacity: 0;
+  }
+}
+</style>
